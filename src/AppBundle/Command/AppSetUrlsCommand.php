77d541ae99261edd20131b549db5f0fdfbf86734
Mikhail Syniuk <msyniuk@gmail.com>
2017-10-21 16:20:52 +0000

    add comman app:set-urls

---
+++ src/AppBundle/Command/AppSetUrlsCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Command;
+
+use GuzzleHttp\Client;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class AppSetUrlsCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:set-urls')
+            ->setDescription('Заполняет таблицу адресами для парсинга')
+            ->addArgument('argument', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $argument = $input->getArgument('argument');
+
+        if ($input->getOption('option')) {
+            // ...
+        }
+        /* @var $http_request Client*/
+        $http_request = $this->getContainer()->get('guzzle.client.guzzle.parse.start_point');
+        $result = $http_request->get('')->getBody();
+        $output->writeln('Command result.' . $result);
+    }
+
+}
